031d640d3a76bd8dc596c7abfaaa220d89529bf2
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-11-27 11:32:20 +0800

    dada

--- models/userModel.js
+++ models/userModel.js
@@ -3,26 +3,21 @@ const mongoose = require("mongoose");
 const studentsSchema = mongoose.Schema({
   username: {
     type: String,
-    required: [true, "Please enter username"],
   },
   lrn: {
     type: String,
-    required: [true, "Please enter lrn"],
   },
 
   firstName: {
     type: String,
-    required: [true, "Please enter firstName"],
   },
 
   middleName: {
     type: String,
-    required: [true, "Please enter middleName"],
   },
 
   lastName: {
     type: String,
-    required: [true, "Please enter lastName"],
   },
   rfid: {
     type: String,
